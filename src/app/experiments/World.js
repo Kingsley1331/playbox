946db855eca18e54c75245bc8283352af79d5455
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2024-12-30 12:38:10 +0000

    Add experiments files and folders

---
+++ src/app/experiments/World.js
@@ -0,0 +1,31 @@
+import planck, { Vec2 } from "planck";
+
+const Scene = {};
+
+const fixtures = [];
+
+const world = new planck.World(new Vec2(0, -10));
+
+Scene.fixtures = fixtures;
+Scene.world = world;
+
+export function addFixture(fixture) {
+  Scene.fixtures.push(fixture);
+  //   Scene.fixtures = [...Scene.fixtures, { ...fixture }];
+}
+
+// export function addFixtures(world) {
+//   const bodies = world.getBodyList();
+//   for (let b = bodies; b; b = b.getNext()) {
+//     console.log("bodies", b);
+//     for (let f = b.getFixtureList(); f; f = f.getNext()) {
+//       console.log("fixtures", f);
+//       Scene.fixtures = push(f);
+//       // for (let v = f.getShape().getVertices(); v; v = v.getNext()) {
+//       //   console.log("vertices", v);
+//       // }
+//     }
+//   }
+// }
+
+export { Scene };
---
+++ src/app/experiments/rendering.js
@@ -0,0 +1,106 @@
+"use client";
+import { Scene } from "./World";
+console.log("rendering123");
+export function render2(
+  world,
+  ctxRef,
+  scale,
+  canvasRef,
+  translation = { x: canvas.width / 2, y: canvas.height / 2 },
+  isPausedRef
+) {
+  const canvas = canvasRef.current;
+  if (world && ctxRef.current) {
+    // const world = worldRef.current;
+    const ctx = ctxRef.current;
+    const frameRate = isPausedRef.current ? Infinity : 60;
+
+    world?.step(1 / frameRate);
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Setup transform
+    ctx.save();
+    ctx.translate(translation.x, translation.y);
+    ctx.scale(1, -1);
+    ctx.scale(scale, scale);
+
+    // Draw all bodies
+    for (let b = world?.getBodyList(); b; b = b.getNext()) {
+      drawBody(ctx, b, scale);
+    }
+
+    ctx.restore();
+    if (!isPausedRef.current) {
+      requestAnimationFrame(() =>
+        render2(
+          world,
+          ctxRef,
+          scale,
+          canvasRef,
+          { x: translation.x, y: translation.y },
+          isPausedRef
+        )
+      );
+    }
+  }
+}
+
+export function drawBody(ctx, body, scale) {
+  // console.log("Scene.fixtures", Scene.fixtures);
+  const colour = "rgb(175, 224, 230, 0.8)";
+  // console.log("Scene.fixtures[0]", Scene.fixtures[0]);
+  // const colour = "rgba(196, 203, 2000, 0.5)";
+  for (let f = body.getFixtureList(); f; f = f.getNext()) {
+    // for (let f of Scene.fixtures) {
+    const shape = f.getShape();
+    const type = shape.getType();
+    const pos = body.getPosition();
+    const angle = body.getAngle();
+    ctx.save();
+    ctx.translate(pos.x, pos.y);
+    ctx.rotate(angle);
+
+    ctx.fillStyle = colour;
+    // ctx.fillStyle = "transparent";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 0.3 / scale;
+
+    // console.log("=========>type", type);
+
+    if (type === "polygon") {
+      // console.log("=========>polygon");
+      ctx.beginPath();
+      const vertices = shape.m_vertices;
+
+      ctx.moveTo(vertices[0].x, vertices[0].y);
+      for (let i = 1; i < vertices.length; i++) {
+        ctx.lineTo(vertices[i].x, vertices[i].y);
+      }
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+    } else if (type === "circle") {
+      // console.log("=========>circle");
+      ctx.beginPath();
+      ctx.arc(shape.m_p.x, shape.m_p.y, shape.m_radius, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.stroke();
+    } else if (type === "edge") {
+      // console.log("=========>edge");
+      ctx.beginPath();
+      ctx.moveTo(shape.m_vertex1.x, shape.m_vertex1.y);
+      ctx.lineTo(shape.m_vertex2.x, shape.m_vertex2.y);
+      ctx.stroke();
+    } else if (type === "chain") {
+      // console.log("=========>chain");
+      ctx.beginPath();
+      const vertices = shape.m_vertices;
+      ctx.moveTo(vertices[0].x, vertices[0].y);
+      for (let i = 1; i < vertices.length; i++) {
+        ctx.lineTo(vertices[i].x, vertices[i].y);
+      }
+      ctx.stroke();
+    }
+    ctx.restore();
+  }
+}
