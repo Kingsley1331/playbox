d41d217dde41382a56bf173313c3b30403ff2481
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2025-01-04 17:42:13 +0000

    Fixed polyline tracking bug

--- src/app/experiments/helpers/utilities.js
+++ src/app/experiments/helpers/utilities.js
@@ -1,10 +1,9 @@
 import { Scene } from "../World";
 import { dragShape, moveShape, throwShape } from "../events/canvas/handlers";
 
-let { scale, world, mousePos } = Scene;
+let { scale, world } = Scene;
 
 export const canvasMouseEvents = (canvas, setMousePosUI) => {
-  //   console.log("canvas", canvas);
   const mouse = {
     x: 0,
     y: 0,
@@ -15,7 +14,7 @@ export const canvasMouseEvents = (canvas, setMousePosUI) => {
     mouse.x = (e.clientX - rect.left) / scale;
     mouse.y = (e.clientY - rect.top) / scale;
     setMousePosUI({ x: mouse.x, y: mouse.y });
-    mousePos = { x: mouse.x, y: mouse.y };
+    Scene.mousePos = { x: mouse.x, y: mouse.y };
 
     moveShape(e, rect);
   };
